cad2b7b7286495858ebe123378a7d7d067f92135
anton <mrsuh6@gmail.com>
2017-08-28 22:11:57 +0300

    source

--- src/AppBundle/Command/UserCommand.php
+++ src/AppBundle/Command/UserCommand.php
@@ -5,30 +5,54 @@ namespace AppBundle\Command;
 use AppBundle\Document\User\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UserCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
-        $this->setName('app:user');
+        $this->setName('app:user')
+            ->addOption(
+                'username',
+                'u',
+                InputOption::VALUE_OPTIONAL
+            )
+            ->addOption(
+                'password',
+                'p',
+                InputOption::VALUE_OPTIONAL
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $dm_user = $this->getContainer()->get('dm.control')->init(User::class);
+        $username = $input->getOption('username');
+        $password = $input->getOption('password');
+        $dm_user  = $this->getContainer()->get('dm.control')->init(User::class);
 
-        $user =
-            (new User())
-                ->setUsername('admin')
-                ->setRoles(['ROLE_ADMIN'])
-                ->setSalt('salt');
+        $exists = $dm_user->findOne(['username' => $username]);
+
+        if (null === $exists) {
+            $user =
+                (new User())
+                    ->setUsername($username)
+                    ->setRoles(['ROLE_ADMIN'])
+                    ->setSalt(md5(time()));
+        } else {
+            $user = $exists;
+        }
 
         $encoder = $this->getContainer()->get('security.password_encoder');
 
-        $pass = $encoder->encodePassword($user, 'password');
+        $pass = $encoder->encodePassword($user, $password);
 
         $user->setPassword($pass);
-        $dm_user->insert($user);
+
+        if (null === $exists) {
+            $dm_user->insert($user);
+        } else {
+            $dm_user->update($user);
+        }
     }
 }
