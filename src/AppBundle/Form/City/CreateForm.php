c4567c4ba1c72efaeea764e99b4a4da76e8f7621
anton <mrsuh6@gmail.com>
2017-09-13 22:21:33 +0300

    city has_subway

--- src/AppBundle/Form/City/CreateForm.php
+++ src/AppBundle/Form/City/CreateForm.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form\City;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -30,6 +31,13 @@ class CreateForm extends AbstractType
             )->add(
                 'picture_link',
                 TextType::class,
+                [
+                    'required'    => true,
+                    'constraints' => [new NotBlank()]
+                ]
+            )->add(
+                'has_subway',
+                CheckboxType::class,
                 [
                     'required'    => true,
                     'constraints' => [new NotBlank()]
